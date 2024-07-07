1e3ebb57bf091ea1d50bb618407c2a4a7fbc72ac
Patrick Stearns <pstearns@miva.com>
2024-07-07 15:25:59 -0700

    Added filesystem FileNotFoundException exception and updated Illuminate references to exception

---
+++ src/Framework/Contracts/Filesystem/FileNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVPS\Lumis\Framework\Contracts\Filesystem;
+
+use Exception;
+
+class FileNotFoundException extends Exception
+{
+}
--- src/Framework/Filesystem/Filesystem.php
+++ src/Framework/Filesystem/Filesystem.php
@@ -3,7 +3,68 @@
 namespace MVPS\Lumis\Framework\Filesystem;
 
 use Illuminate\Filesystem\Filesystem as IlluminateFilesystem;
+use MVPS\Lumis\Framework\Contracts\Filesystem\FileNotFoundException;
+use Throwable;
 
 class Filesystem extends IlluminateFilesystem
 {
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @throws \MVPS\Lumis\Framework\Contracts\Filesystem\FileNotFoundException
+	 */
+	#[\Override]
+	public function get($path, $lock = false)
+	{
+		try {
+			return parent::get($path, $lock);
+		} catch (Throwable $e) {
+			throw new FileNotFoundException($e->getMessage());
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @throws \MVPS\Lumis\Framework\Contracts\Filesystem\FileNotFoundException
+	 */
+	#[\Override]
+	public function getRequire($path, array $data = [])
+	{
+		try {
+			return parent::getRequire($path, $data);
+		} catch (Throwable $e) {
+			throw new FileNotFoundException($e->getMessage());
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @throws \MVPS\Lumis\Framework\Contracts\Filesystem\FileNotFoundException
+	 */
+	#[\Override]
+	public function lines($path)
+	{
+		try {
+			return parent::lines($path);
+		} catch (Throwable $e) {
+			throw new FileNotFoundException($e->getMessage());
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @throws \MVPS\Lumis\Framework\Contracts\Filesystem\FileNotFoundException
+	 */
+	#[\Override]
+	public function requireOnce($path, array $data = [])
+	{
+		try {
+			return parent::requireOnce($path, $data);
+		} catch (Throwable $e) {
+			throw new FileNotFoundException($e->getMessage());
+		}
+	}
 }
