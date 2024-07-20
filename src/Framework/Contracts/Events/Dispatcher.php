b29148690e3b410904f9c5f8d0aaaa6b362f1f05
Patrick Stearns <pstearns@miva.com>
2024-07-20 10:04:49 -0700

    Added event dispatcher contract

---
+++ src/Framework/Contracts/Events/Dispatcher.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVPS\Lumis\Framework\Contracts\Events;
+
+use Illuminate\Contracts\Events\Dispatcher as IlluminateDispatcherContract;
+
+interface Dispatcher extends IlluminateDispatcherContract
+{
+}
