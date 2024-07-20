1986b1fa0db67bc2a1b4f33a60de304643bb744a
Patrick Stearns <pstearns@miva.com>
2024-07-20 10:19:05 -0700

    Removed unneeded method overrides

--- src/Framework/Events/Dispatcher.php
+++ src/Framework/Events/Dispatcher.php
@@ -3,10 +3,8 @@
 namespace MVPS\Lumis\Framework\Events;
 
 use Illuminate\Events\Dispatcher as IlluminateEventDispatcher;
-use MVPS\Lumis\Framework\Collections\Arr;
 use MVPS\Lumis\Framework\Container\Container;
 use MVPS\Lumis\Framework\Contracts\Events\Dispatcher as DispatcherContract;
-use MVPS\Lumis\Framework\Support\Str;
 
 class Dispatcher extends IlluminateEventDispatcher implements DispatcherContract
 {
@@ -24,27 +22,5 @@ class Dispatcher extends IlluminateEventDispatcher implements DispatcherContract
 	public function __construct(Container|null $container = null)
 	{
 		$this->container = $container ?: new Container;
-	}
-
-	/**
-	 * {@inheritdoc}
-	 */
-	#[\Override]
-	protected function parseClassCallable($listener): array
-	{
-		return Str::parseCallback($listener, 'handle');
-	}
-
-	/**
-	 * {@inheritdoc}
-	 */
-	#[\Override]
-	protected function parseEventAndPayload(mixed $event, mixed $payload): array
-	{
-		if (is_object($event)) {
-			[$payload, $event] = [[$event], get_class($event)];
-		}
-
-		return [$event, Arr::wrap($payload)];
 	}
 }
