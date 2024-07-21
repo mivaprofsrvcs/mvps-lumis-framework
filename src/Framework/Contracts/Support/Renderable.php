36d94a47ee7125b21e0a052f9f7dcbc580eea407
Patrick Stearns <pstearns@miva.com>
2024-07-21 15:54:24 -0700

    Added renderable support contract

---
+++ src/Framework/Contracts/Support/Renderable.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVPS\Lumis\Framework\Contracts\Support;
+
+use Illuminate\Contracts\Support\Renderable as IlluminateRenderable;
+
+interface Renderable extends IlluminateRenderable
+{
+}
