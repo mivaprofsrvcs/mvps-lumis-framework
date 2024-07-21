b6c7f3f2cd0f36108e3f2ec36130ab69fdc8bd7b
Patrick Stearns <pstearns@miva.com>
2024-07-21 16:11:38 -0700

    Added ResponseTrait and Renderable content handling

--- src/Framework/Http/ResponseFactory.php
+++ src/Framework/Http/ResponseFactory.php
@@ -2,25 +2,32 @@
 
 namespace MVPS\Lumis\Framework\Http;
 
+use MVPS\Lumis\Framework\Contracts\Support\Renderable;
 use MVPS\Lumis\Framework\Http\Traits\InteractsWithContent;
+use MVPS\Lumis\Framework\Http\Traits\ResponseTrait;
 use pdeans\Http\Factories\ResponseFactory as BaseResponseFactory;
 use pdeans\Http\Factories\StreamFactory;
 
 class ResponseFactory extends BaseResponseFactory
 {
 	use InteractsWithContent;
+	use ResponseTrait;
 
 	/**
 	 * Create a new response instance.
 	 */
 	public function make(mixed $content = '', $status = 200, array $headers = []): Response
 	{
+		$this->original = $content;
+
 		$defaultContentType = 'text/html';
 
 		if ($this->shouldBeJson($content)) {
 			$content = $this->transformToJson($content);
 
 			$defaultContentType = 'application/json';
+		} elseif ($content instanceof Renderable) {
+			$content = $content->render();
 		} else {
 			$content = (string) $content;
 		}
