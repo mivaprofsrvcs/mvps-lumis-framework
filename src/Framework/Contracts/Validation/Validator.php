6976d6f9d37f372567be41a1ea1cedd6e9dfedf6
Patrick Stearns <pstearns@miva.com>
2024-07-23 08:23:23 -0700

    Added validator contract and updated validator to implement contract

---
+++ src/Framework/Contracts/Validation/Validator.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVPS\Lumis\Framework\Contracts\Validation;
+
+use Illuminate\Contracts\Validation\Validator as IlluminateValidator;
+
+interface Validator extends IlluminateValidator
+{
+}
--- src/Framework/Validation/Validator.php
+++ src/Framework/Validation/Validator.php
@@ -3,7 +3,8 @@
 namespace MVPS\Lumis\Framework\Validation;
 
 use Illuminate\Validation\Validator as IlluminateValidator;
+use MVPS\Lumis\Framework\Contracts\Validation\Validator as ValidatorContract;
 
-class Validator extends IlluminateValidator
+class Validator extends IlluminateValidator implements ValidatorContract
 {
 }
