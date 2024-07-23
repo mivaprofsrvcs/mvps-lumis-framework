cb0cb4f160d0bec75ac9c0313c6d41d3164cd544
Patrick Stearns <pstearns@miva.com>
2024-07-23 09:30:47 -0700

    Added more http exception functionality

--- src/Framework/Http/Exceptions/HttpResponseException.php
+++ src/Framework/Http/Exceptions/HttpResponseException.php
@@ -2,8 +2,31 @@
 
 namespace MVPS\Lumis\Framework\Http\Exceptions;
 
+use MVPS\Lumis\Framework\Http\Response;
 use RuntimeException;
 
 class HttpResponseException extends RuntimeException
 {
+	/**
+	 * The underlying response instance.
+	 *
+	 * @var \MVPS\Lumis\Framework\Http\Response
+	 */
+	protected Response $response;
+
+	/**
+	 * Create a new HTTP response exception instance.
+	 */
+	public function __construct(Response $response)
+	{
+		$this->response = $response;
+	}
+
+	/**
+	 * Get the underlying response instance.
+	 */
+	public function getResponse(): Response
+	{
+		return $this->response;
+	}
 }
