10a4b9f020548ec3aace10aff42f39fd65765cc1
Patrick Stearns <pstearns@miva.com>
2024-07-25 16:23:13 -0700

    Added database config file

--- src/Framework/config/database.php
+++ src/Framework/config/database.php
@@ -7,28 +7,25 @@ return [
 	| Default Database Connection Name
 	|--------------------------------------------------------------------------
 	|
-	| Here you may specify which of the database connections below you wish
-	| to use as your default connection for database operations. This is
-	| the connection which will be utilized unless another connection
-	| is explicitly specified when you execute a query / statement.
+	| Specify the default database connection to be used for all database
+	| operations. This connection will be utilized unless an alternative
+	| connection is explicitly specified when executing a query or statement.
 	|
 	*/
-
 	'default' => env('DB_CONNECTION', 'sqlite'),
+
 
 	/*
 	|--------------------------------------------------------------------------
 	| Database Connections
 	|--------------------------------------------------------------------------
 	|
-	| Below are all of the database connections defined for your application.
-	| An example configuration is provided for each database system which
-	| is supported by Laravel. You're free to add / remove connections.
+	| The following section lists all the database connections configured for
+	| your application. Each supported database system in Lumis includes an
+	| example configuration. Feel free to add or remove connections as needed.
 	|
 	*/
-
 	'connections' => [
-
 		'sqlite' => [
 			'driver' => 'sqlite',
 			'url' => env('DB_URL'),
@@ -39,13 +36,12 @@ return [
 			'journal_mode' => null,
 			'synchronous' => null,
 		],
-
 		'mysql' => [
 			'driver' => 'mysql',
 			'url' => env('DB_URL'),
 			'host' => env('DB_HOST', '127.0.0.1'),
 			'port' => env('DB_PORT', '3306'),
-			'database' => env('DB_DATABASE', 'laravel'),
+			'database' => env('DB_DATABASE', 'lumis'),
 			'username' => env('DB_USERNAME', 'root'),
 			'password' => env('DB_PASSWORD', ''),
 			'unix_socket' => env('DB_SOCKET', ''),
@@ -55,17 +51,16 @@ return [
 			'prefix_indexes' => true,
 			'strict' => true,
 			'engine' => null,
-			'options' => extension_loaded('pdo_mysql') ? array_filter([
-				PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-			]) : [],
+			'options' => extension_loaded('pdo_mysql')
+				? array_filter([PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA')])
+				: [],
 		],
-
 		'mariadb' => [
 			'driver' => 'mariadb',
 			'url' => env('DB_URL'),
 			'host' => env('DB_HOST', '127.0.0.1'),
 			'port' => env('DB_PORT', '3306'),
-			'database' => env('DB_DATABASE', 'laravel'),
+			'database' => env('DB_DATABASE', 'lumis'),
 			'username' => env('DB_USERNAME', 'root'),
 			'password' => env('DB_PASSWORD', ''),
 			'unix_socket' => env('DB_SOCKET', ''),
@@ -75,17 +70,16 @@ return [
 			'prefix_indexes' => true,
 			'strict' => true,
 			'engine' => null,
-			'options' => extension_loaded('pdo_mysql') ? array_filter([
-				PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-			]) : [],
+			'options' => extension_loaded('pdo_mysql')
+				? array_filter([PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA')])
+				: [],
 		],
-
 		'pgsql' => [
 			'driver' => 'pgsql',
 			'url' => env('DB_URL'),
 			'host' => env('DB_HOST', '127.0.0.1'),
 			'port' => env('DB_PORT', '5432'),
-			'database' => env('DB_DATABASE', 'laravel'),
+			'database' => env('DB_DATABASE', 'lumis'),
 			'username' => env('DB_USERNAME', 'root'),
 			'password' => env('DB_PASSWORD', ''),
 			'charset' => env('DB_CHARSET', 'utf8'),
@@ -94,13 +88,12 @@ return [
 			'search_path' => 'public',
 			'sslmode' => 'prefer',
 		],
-
 		'sqlsrv' => [
 			'driver' => 'sqlsrv',
 			'url' => env('DB_URL'),
 			'host' => env('DB_HOST', 'localhost'),
 			'port' => env('DB_PORT', '1433'),
-			'database' => env('DB_DATABASE', 'laravel'),
+			'database' => env('DB_DATABASE', 'lumis'),
 			'username' => env('DB_USERNAME', 'root'),
 			'password' => env('DB_PASSWORD', ''),
 			'charset' => env('DB_CHARSET', 'utf8'),
@@ -109,7 +102,6 @@ return [
 			// 'encrypt' => env('DB_ENCRYPT', 'yes'),
 			// 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
 		],
-
 	],
 
 	/*
@@ -117,15 +109,13 @@ return [
 	| Migration Repository Table
 	|--------------------------------------------------------------------------
 	|
-	| This table keeps track of all the migrations that have already run for
-	| your application. Using this information, we can determine which of
-	| the migrations on disk haven't actually been run on the database.
+	| This table records the migration history for the application, tracking
+	| which migrations have been executed. This information is used to
+	| determine which migrations need to be run during the migration process.
 	|
 	*/
-
 	'migrations' => [
 		'table' => 'migrations',
 		'update_date_on_publish' => true,
 	],
-
 ];
