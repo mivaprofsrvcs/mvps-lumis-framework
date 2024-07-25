ea9e9ef1e9197683f1790f1c4f16af56c67d2f40
Patrick Stearns <pstearns@miva.com>
2024-07-25 16:07:21 -0700

    Added database connection and connection resolver contracts

---
+++ src/Framework/Contracts/Database/ConnectionResolver.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVPS\Lumis\Framework\Contracts\Database;
+
+use Illuminate\Database\ConnectionResolverInterface;
+
+class ConnectionResolver extends ConnectionResolverInterface
+{
+}
