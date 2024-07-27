3e4188577fd99883fe57a26999cf288e42b5b6ca
Patrick Stearns <pstearns@miva.com>
2024-07-26 18:00:33 -0700

    Added application contract

---
+++ src/Framework/Contracts/Framework/Application.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVPS\Lumis\Framework\Contracts\Framework;
+
+use Illuminate\Contracts\Foundation\Application as IlluminateApplication;
+
+interface Application extends IlluminateApplication
+{
+}
