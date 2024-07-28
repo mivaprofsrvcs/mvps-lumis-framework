9a8ad8e3e544999ee17fea948d3d5418757a8465
Patrick Stearns <pstearns@miva.com>
2024-07-28 08:44:10 -0700

    Added console isolatable contract

---
+++ src/Framework/Contracts/Console/Isolatable.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVPS\Lumis\Framework\Contracts\Console;
+
+use Illuminate\Contracts\Console\Isolatable as IlluminateIsolatable;
+
+interface Isolatable extends IlluminateIsolatable
+{
+}
