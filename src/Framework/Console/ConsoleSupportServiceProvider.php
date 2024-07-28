b76b3dfc751f491fb4a6bdf268eb0dc018ef2037
Patrick Stearns <pstearns@miva.com>
2024-07-28 10:32:05 -0700

    Added migration service provider to provders list

--- src/Framework/Console/ConsoleSupportServiceProvider.php
+++ src/Framework/Console/ConsoleSupportServiceProvider.php
@@ -3,6 +3,7 @@
 namespace MVPS\Lumis\Framework\Console;
 
 use MVPS\Lumis\Framework\Contracts\Support\DeferrableProvider;
+use MVPS\Lumis\Framework\Database\MigrationServiceProvider;
 use MVPS\Lumis\Framework\Providers\AggregateServiceProvider;
 use MVPS\Lumis\Framework\Support\ComposerServiceProvider;
 
@@ -11,10 +12,11 @@ class ConsoleSupportServiceProvider extends AggregateServiceProvider implements 
 	/**
 	 * The provider class names.
 	 *
-	 * @var string[]
+	 * @var array<string>
 	 */
 	protected array $providers = [
 		LumisServiceProvider::class,
+		MigrationServiceProvider::class,
 		ComposerServiceProvider::class,
 	];
 }
