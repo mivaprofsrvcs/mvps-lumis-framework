8bb858a371c11ea477c6e9f247650a4ddc753033
Patrick Stearns <pstearns@miva.com>
2024-07-29 14:51:08 -0700

    Updated container property to reference container contract and added getMiddleware method

--- src/Framework/Routing/ControllerDispatcher.php
+++ src/Framework/Routing/ControllerDispatcher.php
@@ -2,19 +2,21 @@
 
 namespace MVPS\Lumis\Framework\Routing;
 
-use MVPS\Lumis\Framework\Container\Container;
+use MVPS\Lumis\Framework\Contracts\Container\Container;
 use MVPS\Lumis\Framework\Contracts\Routing\ControllerDispatcher as ControllerDispatcherContract;
 use MVPS\Lumis\Framework\Routing\Route;
+use MVPS\Lumis\Framework\Routing\Traits\FiltersControllerMiddleware;
 use MVPS\Lumis\Framework\Routing\Traits\ResolvesRouteDependencies;
 
 class ControllerDispatcher implements ControllerDispatcherContract
 {
+	use FiltersControllerMiddleware;
 	use ResolvesRouteDependencies;
 
 	/**
 	 * The container instance.
 	 *
-	 * @var \MVPS\Lumis\Framework\Container\Container
+	 * @var \MVPS\Lumis\Framework\Contracts\Container\Container
 	 */
 	protected Container $container;
 
@@ -46,5 +48,22 @@ class ControllerDispatcher implements ControllerDispatcherContract
 	protected function resolveParameters(Route $route, mixed $controller, string $method): array
 	{
 		return $this->resolveClassMethodDependencies($route->parametersWithoutNulls(), $controller, $method);
+	}
+
+	/**
+	 * Get the middleware for the controller instance.
+	 */
+	public function getMiddleware(Controller $controller, string $method): array
+	{
+		if (! method_exists($controller, 'getMiddleware')) {
+			return [];
+		}
+
+		return collection($controller->getMiddleware())
+			->reject(function ($data) use ($method) {
+				return static::methodExcludedByOptions($method, $data['options']);
+			})
+			->pluck('middleware')
+			->all();
 	}
 }
