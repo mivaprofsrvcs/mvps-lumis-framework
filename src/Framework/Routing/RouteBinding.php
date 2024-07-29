d7b80e395da30e40fb975eba9eea51638b2cd7e3
Patrick Stearns <pstearns@miva.com>
2024-07-29 10:53:11 -0700

    Added route binding class

---
+++ src/Framework/Routing/RouteBinding.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace MVPS\Lumis\Framework\Routing;
+
+use Closure;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use MVPS\Lumis\Framework\Contracts\Container\Container;
+use MVPS\Lumis\Framework\Support\Str;
+
+class RouteBinding
+{
+	/**
+	 * Create a class based binding using the IoC container.
+	 */
+	protected static function createClassBinding(Container $container, string $binding): Closure
+	{
+		return function ($value, $route) use ($container, $binding) {
+			// If the binding contains an '@' sign, it indicates that the class
+			// name is being delimited from the bind method name. This allows
+			// for bindings to execute multiple bind methods within a single
+			// class for convenience.
+			[$class, $method] = Str::parseCallback($binding, 'bind');
+
+			$callable = [$container->make($class), $method];
+
+			return $callable($value, $route);
+		};
+	}
+
+	/**
+	 * Create a Route model binding for a given callback.
+	 */
+	public static function forCallback(Container $container, Closure|string $binder): Closure
+	{
+		return is_string($binder)
+			? static::createClassBinding($container, $binder)
+			: $binder;
+	}
+
+	/**
+	 * Create a Route model binding for a model.
+	 *
+	 * @throws \Illuminate\Database\Eloquent\ModelNotFoundException<\Illuminate\Database\Eloquent\Model>
+	 */
+	public static function forModel(Container $container, string $class, Closure|null $callback = null): Closure
+	{
+		return function ($value, $route) use ($container, $class, $callback) {
+			if (is_null($value)) {
+				return;
+			}
+
+			// Attempt to retrieve models using the first method on the model
+			// instance.  If retrieval fails, throw a model not found exception;
+			// otherwise, return the instance.
+			$instance = $container->make($class);
+
+			$routeBindingMethod = $route->allowsTrashedBindings() &&
+				in_array(SoftDeletes::class, class_uses_recursive($instance))
+					? 'resolveSoftDeletableRouteBinding'
+					: 'resolveRouteBinding';
+
+			if ($model = $instance->{$routeBindingMethod}($value)) {
+				return $model;
+			}
+
+			// If a callback was provided, invoke it to determine the behavior
+			// when the  model is not found. This offers flexibility in handling
+			// missing models.
+			if ($callback instanceof Closure) {
+				return $callback($value);
+			}
+
+			throw (new ModelNotFoundException)->setModel($class);
+		};
+	}
+}
