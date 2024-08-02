fc01464196d29db7c01bbad4cf590b70ab2cabd3
Patrick Stearns <pstearns@miva.com>
2024-08-02 07:08:44 -0700

    Added driver and manager registration

--- src/Framework/Filesystem/FilesystemServiceProvider.php
+++ src/Framework/Filesystem/FilesystemServiceProvider.php
@@ -7,11 +7,43 @@ use MVPS\Lumis\Framework\Providers\ServiceProvider;
 class FilesystemServiceProvider extends ServiceProvider
 {
 	/**
+	 * Get the default file driver.
+	 */
+	protected function getDefaultDriver(): string
+	{
+		return $this->app['config']['filesystems.default'];
+	}
+
+	/**
 	 * Register the filesystem service provider.
 	 */
 	public function register(): void
 	{
 		$this->registerNativeFilesystem();
+
+		$this->registerDrivers();
+	}
+
+	/**
+	 * Register the driver based filesystem.
+	 */
+	protected function registerDrivers(): void
+	{
+		$this->registerManager();
+
+		$this->app->singleton('filesystem.disk', function ($app) {
+			return $app['filesystem']->disk($this->getDefaultDriver());
+		});
+	}
+
+	/**
+	 * Register the filesystem manager.
+	 */
+	protected function registerManager(): void
+	{
+		$this->app->singleton('filesystem', function ($app) {
+			return new FilesystemManager($app);
+		});
 	}
 
 	/**
