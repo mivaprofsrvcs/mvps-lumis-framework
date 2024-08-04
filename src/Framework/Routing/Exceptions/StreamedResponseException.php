6b3203a509515d297036b415d5ac907ef330585f
Patrick Stearns <pstearns@miva.com>
2024-08-04 13:49:39 -0700

    Added routing streamed response exception

---
+++ src/Framework/Routing/Exceptions/StreamedResponseException.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace MVPS\Lumis\Framework\Routing\Exceptions;
+
+use MVPS\Lumis\Framework\Http\Response;
+use RuntimeException;
+use Throwable;
+
+class StreamedResponseException extends RuntimeException
+{
+	/**
+	 * The actual exception thrown during the stream.
+	 *
+	 * @var Throwable
+	 */
+	public $originalException;
+
+	/**
+	 * Create a new streamed response exception instance.
+	 */
+	public function __construct(Throwable $originalException)
+	{
+		$this->originalException = $originalException;
+
+		parent::__construct($originalException->getMessage());
+	}
+
+	/**
+	 * Get the actual exception thrown during the stream.
+	 *
+	 * @return \Throwable
+	 */
+	public function getInnerException()
+	{
+		return $this->originalException;
+	}
+
+	/**
+	 * Render the exception.
+	 */
+	public function render(): Response
+	{
+		return new Response('');
+	}
+}
