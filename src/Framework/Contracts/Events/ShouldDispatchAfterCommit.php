1d09bc98debed525d76ef79948c39ad32b8d704c
Patrick Stearns <pstearns@miva.com>
2024-08-25 10:47:45 -0700

    Added contracts for after commit events

---
+++ src/Framework/Contracts/Events/ShouldDispatchAfterCommit.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVPS\Lumis\Framework\Contracts\Events;
+
+use Illuminate\Contracts\Events\ShouldDispatchAfterCommit as ShouldDispatchAfterCommitContract;
+
+interface ShouldDispatchAfterCommit extends ShouldDispatchAfterCommitContract
+{
+}
---
+++ src/Framework/Contracts/Events/ShouldHandleEventsAfterCommit.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVPS\Lumis\Framework\Contracts\Events;
+
+use Illuminate\Contracts\Events\ShouldHandleEventsAfterCommit as ShouldHandleEventsAfterCommitContract;
+
+interface ShouldHandleEventsAfterCommit extends ShouldHandleEventsAfterCommitContract
+{
+}
